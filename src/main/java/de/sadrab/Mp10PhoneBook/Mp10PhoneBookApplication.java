ce8deb8d09a05fc584f1cda55ed56a441981966d
Sadra <ti9mail@gmail.com>
2018-04-04 18:12:27 +0200

    Add Spring structure

---
+++ src/main/java/de/sadrab/Mp10PhoneBook/Mp10PhoneBookApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp10PhoneBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp10PhoneBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp10PhoneBookApplication.class, args);
+	}
+}
---
+++ src/test/java/de/sadrab/Mp10PhoneBook/Mp10PhoneBookApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sadrab.Mp10PhoneBook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp10PhoneBookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
